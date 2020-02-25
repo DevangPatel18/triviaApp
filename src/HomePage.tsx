947d58517d788bd931f85748f3ecfbf621854ddd
dev637 <devang713@gmail.com>
2020-02-25 11:19:55 -0500

    Add useFormik hook to setup form state

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,56 +1,131 @@
 import React from 'react';
-import { Store } from './Store'
-import categories from './categories'
+import { Store } from './Store';
+import categoryList from './categories';
+import { useFormik } from 'formik';
 
 const HomePage = () => {
-  const { state, dispatch } = React.useContext(Store)
+  const { state, dispatch } = React.useContext(Store);
+  const formik = useFormik({
+    initialValues: {
+      amount: 5,
+      categories: 9,
+      difficulty: '',
+      type: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  const { values, handleSubmit, handleChange } = formik;
+  const { amount, difficulty, type } = values;
 
   return (
     <React.Suspense fallback={<div>loading</div>}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <p>Number of Questions</p>
-        <input type="range" name="amount" min="5" max="50" step="5" />
+        <input
+          type="range"
+          name="amount"
+          min="5"
+          max="50"
+          step="5"
+          value={amount}
+          onChange={handleChange}
+        />
         <p>Categories</p>
-        <select name="categories">
-          {categories.map(({ id, name }) => <option value={id} key={id}>{name}</option>)}
+        <select name="categories" onChange={handleChange}>
+          {categoryList.map(({ id, name }) => (
+            <option value={id} key={id}>
+              {name}
+            </option>
+          ))}
         </select>
         <p>Difficulty</p>
         <ul>
           <li>
-            <input type="radio" name="difficulty" value="" id="anyDifficulty" />
+            <input
+              type="radio"
+              name="difficulty"
+              value=""
+              id="anyDifficulty"
+              checked={difficulty === ''}
+              onChange={handleChange}
+            />
             <label htmlFor="anyDifficulty">Any</label>
           </li>
           <li>
-            <input type="radio" name="difficulty" value="easy" id="easy" />
+            <input
+              type="radio"
+              name="difficulty"
+              value="easy"
+              id="easy"
+              checked={difficulty === 'easy'}
+              onChange={handleChange}
+            />
             <label htmlFor="easy">Easy</label>
           </li>
           <li>
-            <input type="radio" name="difficulty" value="medium" id="medium" />
+            <input
+              type="radio"
+              name="difficulty"
+              value="medium"
+              id="medium"
+              checked={difficulty === 'medium'}
+              onChange={handleChange}
+            />
             <label htmlFor="medium">Medium</label>
           </li>
           <li>
-            <input type="radio" name="difficulty" value="hard" id="hard" />
+            <input
+              type="radio"
+              name="difficulty"
+              value="hard"
+              id="hard"
+              checked={difficulty === 'hard'}
+              onChange={handleChange}
+            />
             <label htmlFor="hard">Hard</label>
           </li>
         </ul>
         <p>Type</p>
         <ul>
           <li>
-            <input type="radio" name="type" value="" id="anyType" />
+            <input
+              type="radio"
+              name="type"
+              value=""
+              id="anyType"
+              checked={type === ''}
+              onChange={handleChange}
+            />
             <label htmlFor="anyType">Any</label>
           </li>
           <li>
-            <input type="radio" name="type" value="multiple" id="multiple" />
+            <input
+              type="radio"
+              name="type"
+              value="multiple"
+              id="multiple"
+              checked={type === 'multiple'}
+              onChange={handleChange}
+            />
             <label htmlFor="multiple">Multiple</label>
           </li>
           <li>
-            <input type="radio" name="type" value="boolean" id="boolean" />
+            <input
+              type="radio"
+              name="type"
+              value="boolean"
+              id="boolean"
+              checked={type === 'boolean'}
+              onChange={handleChange}
+            />
             <label htmlFor="boolean">Boolean</label>
           </li>
         </ul>
         <button type="submit">Start Quiz</button>
       </form>
-    </React.Suspense >
+    </React.Suspense>
   );
 };
 
