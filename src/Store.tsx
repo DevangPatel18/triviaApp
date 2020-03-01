0cbded2c50203f042087297d96f4437c96d8fde3
dev637 <devang713@gmail.com>
2020-03-01 10:45:29 -0500

    Update Store with fetch questions action

--- src/Store.tsx
+++ src/Store.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { IState, IAction } from './interfaces'
 
 const initialState: IState = {
+  questions: []
 }
 
 export const Store = React.createContext<IState | any>(initialState)
 
 function reducer(state: IState, action: IAction): IState {
   switch (action.type) {
+    case 'FETCH_QUESTIONS':
+      return {...state, questions: action.payload }
     default:
       return state
   }
