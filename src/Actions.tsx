7c8eab8e45dcfcce9cf823c52035d554640b4ae3
dev637 <devang713@gmail.com>
2020-03-01 10:57:01 -0500

    Add fetchQuestions action

---
+++ src/Actions.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, IQuizConfigForm } from './interfaces';
+
+export const fetchQuestions = async (
+  formState: IQuizConfigForm,
+  dispatch: Dispatch
+) => {
+  const queryParams = Object.entries(formState)
+    .filter(([_, value]) => value)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
+  const fetchUrl = `https://opentdb.com/api.php?${queryParams}`;
+  const response = await fetch(fetchUrl).then(res => res.json());
+  if (response.response_code === 0) {
+    dispatch({
+      type: 'FETCH_QUESTIONS',
+      payload: response.results,
+    });
+  }
+};
