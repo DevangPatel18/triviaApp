1c69ca54db0a733fd2c5dece24ffb8d2b083f6cd
dev637 <devang713@gmail.com>
2020-03-05 12:32:57 -0500

    Refactor fetchQuestions with axios

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -1,15 +1,16 @@
 import { Dispatch, IQuizConfigForm } from './interfaces';
+import axios from 'axios';
+
+const fetchUrl = `https://opentdb.com/api.php`;
 
 export const fetchQuestions = async (
   formState: IQuizConfigForm,
   dispatch: Dispatch
 ) => {
-  const queryParams = Object.entries(formState)
-    .filter(([_, value]) => value)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
-  const fetchUrl = `https://opentdb.com/api.php?${queryParams}`;
-  const response = await fetch(fetchUrl).then(res => res.json());
+  const response = await axios(fetchUrl, {
+    params: formState,
+    timeout: 2000,
+  }).then(res => res.data);
   if (response.response_code === 0) {
     dispatch({
       type: 'FETCH_QUESTIONS',
