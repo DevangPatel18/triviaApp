b8c061779f1927335bc117f490deed5c686edade
dev637 <devang713@gmail.com>
2020-03-07 11:36:56 -0500

    Add cancelQuiz action

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import { IState, IAction } from './interfaces'
+import React from 'react';
+import { IState, IAction } from './interfaces';
 
 const initialState: IState = {
   questions: [],
-  loadStatus : false,
+  loadStatus: false,
   answers: [],
   isQuizActive: false,
   currentQuestion: 0,
-}
+};
 
-export const Store = React.createContext<IState | any>(initialState)
+export const Store = React.createContext<IState | any>(initialState);
 
 function reducer(state: IState, action: IAction): IState {
   switch (action.type) {
     case 'FETCH_QUESTIONS':
-      return {...state, questions: action.payload }
+      return { ...state, questions: action.payload, isQuizActive: true };
     case 'SET_LOAD_STATUS':
-      return {...state, loadStatus: action.payload }
+      return { ...state, loadStatus: action.payload };
+    case 'CANCEL_QUIZ':
+      return {
+        ...state,
+        questions: [],
+        isQuizActive: false,
+        currentQuestion: 0,
+      };
     default:
-      return state
+      return state;
   }
 }
 
-export function StoreProvider({ children }: JSX.ElementChildrenAttribute): JSX.Element {
-  const [state, dispatch] = React.useReducer(reducer, initialState)
-  return <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+export function StoreProvider({
+  children,
+}: JSX.ElementChildrenAttribute): JSX.Element {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
 }
