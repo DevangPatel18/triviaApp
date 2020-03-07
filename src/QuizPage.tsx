e5c7fabd2a1b0e36994867ce057ea862b6e43d32
dev637 <devang713@gmail.com>
2020-03-07 12:19:53 -0500

    Add cancel button to QuizPage

--- src/QuizPage.tsx
+++ src/QuizPage.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { Store } from './Store';
 import categoryList from './categories';
 import { Formik } from 'formik';
+import { navigate } from '@reach/router';
+import { cancelQuiz } from './Actions';
 
 const QuizPage = () => {
   const { state, dispatch } = React.useContext(Store);
 
+  if (!state.isQuizActive) {
+    navigate('/');
+  }
+
   return (
     <React.Suspense fallback={<div>loading</div>}>
-      <section className="quizMenu">
-        Quiz question
+      <section className="quizQuestion container">
+        <button
+          className="quizQuestion_cancelButton"
+          onClick={() => cancelQuiz(dispatch)}
+        >
+          ✖
+        </button>
       </section>
     </React.Suspense>
   );
