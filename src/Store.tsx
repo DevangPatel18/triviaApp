50462deac90007285244117aa459baa1cb70bf0c
dev637 <devang713@gmail.com>
2020-03-09 17:18:16 -0400

    Add choices property to questions
    Add choices property to questions for randomizing choice order.

--- src/Store.tsx
+++ src/Store.tsx
@@ -14,6 +14,18 @@ export const Store = React.createContext<IState | any>(initialState);
 function reducer(state: IState, action: IAction): IState {
   switch (action.type) {
     case 'FETCH_QUESTIONS':
+      action.payload.forEach(obj => {
+        if (obj.type === 'multiple') {
+          const indexes = [0, 1, 2, 3];
+          for (let i = indexes.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [indexes[i], indexes[j]] = [indexes[j], indexes[i]];
+          }
+          obj.choices = indexes;
+        } else {
+          obj.choices = [];
+        }
+      });
       return { ...state, questions: action.payload, isQuizActive: true };
     case 'SET_LOAD_STATUS':
       return { ...state, loadStatus: action.payload };
