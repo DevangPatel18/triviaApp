0412000910fdfa0881bedca734276c31218bda60
dev637 <devang713@gmail.com>
2020-03-18 13:23:03 -0400

    Restyle header title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Store } from './Store'
+import React from 'react';
+import { Store } from './Store';
 
 export default function App(props: any): JSX.Element {
-  const { state } = React.useContext(Store)
+  const { state } = React.useContext(Store);
 
   return (
     <React.Fragment>
-      <header>
-        <h1 >Trivia App</h1>
+      <header className="quizHeader">
+        <h1>
+          <span className="quizHeader_title">Trivia App</span>
+        </h1>
       </header>
       {props.children}
     </React.Fragment>
-  )
+  );
 }
