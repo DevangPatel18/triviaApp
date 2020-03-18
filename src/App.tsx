9d68acdf8540d82e15d0f1dbd0ecac1cd0a3cc2f
dev637 <devang713@gmail.com>
2020-03-18 13:19:47 -0400

    Remove home link from header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Store } from './Store'
-import { Link } from '@reach/router'
 
 export default function App(props: any): JSX.Element {
   const { state } = React.useContext(Store)
@@ -9,7 +8,6 @@ export default function App(props: any): JSX.Element {
     <React.Fragment>
       <header>
         <h1 >Trivia App</h1>
-        <Link to='/'>Home</Link>
       </header>
       {props.children}
     </React.Fragment>
