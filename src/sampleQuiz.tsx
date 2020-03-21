963e699a397277f8cfc655d1b63062a7dc10aea2
dev637 <devang713@gmail.com>
2020-03-21 12:09:13 -0400

    Create sampleQuiz.tsx

---
+++ src/sampleQuiz.tsx
@@ -0,0 +1,20 @@
+export default [
+  {
+    category: '9',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'What is answer to question a',
+    correct_answer: 'correct_a',
+    incorrect_answers: ['wrong_aa', 'wrong_ab', 'wrong_ac'],
+    choices: [0, 1, 2, 3],
+  },
+  {
+    category: '9',
+    type: 'multiple',
+    difficulty: 'medium',
+    question: 'What is answer to question b',
+    correct_answer: 'correct_b',
+    incorrect_answers: ['wrong_ba', 'wrong_bb', 'wrong_bc'],
+    choices: [3, 2, 1, 9],
+  },
+];
