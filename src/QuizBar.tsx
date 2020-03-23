acdea8ddaed9b8da6d37c19ed19838b68b4d3d99
dev637 <devang713@gmail.com>
2020-03-23 14:00:45 -0400

    Create QuizBar component for displaying progress

---
+++ src/QuizBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Store } from './Store';
+
+const QuizBar = () => {
+  const { state } = React.useContext(Store);
+  const { questions, answers } = state;
+
+  const percentage = 100 * (answers.length / questions.length);
+
+  return (
+    <nav>
+      <div className="quizProgressbar">
+        <span
+          style={{
+            width: `${percentage}%`,
+          }}
+          className="quizProgressbar_fill"
+        >
+          {`${percentage}%`}
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default QuizBar;
